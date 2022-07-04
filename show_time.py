6b99ea3771760260c6ddddb364cc7d77f4e771df
neha-nerurkar <100443180+neha-nerurkar@users.noreply.github.com>
2022-07-03 22:56:53 -0400

    Update show_time.py

--- show_time.py
+++ show_time.py
@@ -3,3 +3,7 @@ from datetime import datetime
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print("Current Time = ", current_time)
+present = now.strftime("%d/%m/%y")
+print("Today's Date = ", present)
+day = datetime.today().strftime("%A")
+print("Day = ", day)
