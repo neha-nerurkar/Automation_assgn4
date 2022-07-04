72bade5607913e86bc90ce8b77fae18b32cb9271
neha-nerurkar <nerurkar.n@northeastern.edu>
2022-07-03 19:32:59 -0700

    date and time added

--- show_time.py
+++ show_time.py
@@ -3,3 +3,5 @@ from datetime import datetime
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print("Current Time = ", current_time)
+present = now.strftime("%d/%m/%y")
+print("Present Date = ", present)
